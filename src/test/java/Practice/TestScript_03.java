510e11e9c3418c4049658245fb159e1f4b36f943
Soumitra94 <soumitradhar53@gmail.com>
2023-09-30 13:23:31 +0530

    Third Commit

---
+++ src/test/java/Practice/TestScript_03.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestScript_03
+{
+	@Test
+	public void TS()
+	{
+		System.out.println("Test Script-03 is running");
+	}
+
+}
