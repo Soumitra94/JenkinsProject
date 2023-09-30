61a653301bfa5508785a703b5ec4591d7b62d2c4
Soumitra94 <soumitradhar53@gmail.com>
2023-09-30 12:39:13 +0530

    1st commit

---
+++ src/test/java/Practice/TestScript_01.java
@@ -0,0 +1,15 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestScript_01
+{
+	@Test
+	public void TS()
+	{
+		System.out.println("---Test Script-01 is running------");
+	}
+	
+	
+
+}
