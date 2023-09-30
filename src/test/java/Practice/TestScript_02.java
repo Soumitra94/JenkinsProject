2ab3c57c42da2514549d1760191f3f1f5c90d198
Soumitra94 <soumitradhar53@gmail.com>
2023-09-30 13:14:46 +0530

    Second Commit

---
+++ src/test/java/Practice/TestScript_02.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class TestScript_02 
+{
+	@Test
+	public void TS()
+	{
+		System.out.println("Test Script-02 is running");
+	}
+
+}
